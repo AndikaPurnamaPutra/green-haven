c990b40ceccdd7b411fc777d5702d2a9ea99d979
Abdul Faqih <abdulfaqih7777@gmail.com>
2023-12-12 20:39:01 +0700

    eslint: improve code quality

--- src/scripts/garden.js
+++ src/scripts/garden.js
@@ -1,11 +1,11 @@
 fetch('https://apiplant.abdulfaqih.eu.org/plant')
-  .then(response => response.json())
-  .then(data => {
-    if (data.status === "success") {
-      const plants = data.data.plants;
-      const plantListContainer = document.getElementById("plant-list");
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.status === 'success') {
+      const { plants } = data.data;
+      const plantListContainer = document.getElementById('plant-list');
 
-      plants.slice(0, 3).forEach(plant => {
+      plants.slice(0, 3).forEach((plant) => {
         const cardHTML = `
       <div class="card">
          <img src="${plant.image.small_url}" class="card-img-top" alt="${plant.common_name}" />
@@ -15,10 +15,9 @@ fetch('https://apiplant.abdulfaqih.eu.org/plant')
             </div>
       </div>`;
         plantListContainer.innerHTML += cardHTML;
-
       });
     } else {
-      console.error("API request failed with message:", data.message);
+      console.error('API request failed with message:', data.message);
     }
   })
-  .catch(error => console.error("Error fetching data from API:", error));
+  .catch((error) => console.error('Error fetching data from API:', error));
