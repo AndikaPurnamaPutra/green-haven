f359ffddb4373f2043843254b3fe021baf7aa8ce
Abdul Faqih <abdulfaqih7777@gmail.com>
2023-12-12 11:24:27 +0700

    --fix: card doesnt appear on mobile

--- src/scripts/home-page.js
+++ src/scripts/home-page.js
@@ -4,11 +4,10 @@ fetch('https://apiplant.abdulfaqih.eu.org/plant')
     if (data.status === "success") {
       const plants = data.data.plants;
       const plantListContainer = document.getElementById("plant-list");
-      let aosDelay = 300; 
 
       plants.slice(0, 3).forEach(plant => {
         const cardHTML = `
-          <div class="card m-0" data-aos="fade-up" data-aos-delay="${aosDelay}">
+          <div class="card m-0">
             <img src="${plant.image.small_url}" class="card-img-top" alt="${plant.common_name}" />
             <div class="card-body">
               <h5 class="card-title">${plant.common_name}</h5>
@@ -17,7 +16,6 @@ fetch('https://apiplant.abdulfaqih.eu.org/plant')
           </div>`;
         plantListContainer.innerHTML += cardHTML;
 
-        aosDelay += 300; 
       });
     } else {
       console.error("API request failed with message:", data.message);
